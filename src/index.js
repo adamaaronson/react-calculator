16dae0b4fa294999ee9fafdd1fdb952bd0fb3ba9
adamaaronson <adamaaronson@gmail.com>
2020-03-16 19:57:39 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,151 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import './styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BUTTONS, BASIC_LAYOUT } from './buttontypes.js';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Display extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            content: this.props.content
+        }
+    }
+    render() {
+        return (
+            <div id="display">
+                <span id="display-text">
+                    {this.props.content}
+                </span>
+            </div>
+        );
+    }
+}
+
+class CalculatorButton extends React.Component {
+    render() {
+        let style = {
+            gridColumn: "auto / span " + this.props.buttonType.width
+        }
+        return (
+            <button className={this.props.buttonType.className} style={style} onClick={() => this.props.onClick()}>
+                {this.props.buttonType.content}
+            </button>
+        );
+    }
+}
+
+class ButtonGrid extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            buttons: []
+        };
+    }
+    renderButton(buttonType) {
+        return (
+            <CalculatorButton
+                buttonType={buttonType}
+                key={buttonType.content}
+                onClick={() => this.props.onClick(buttonType)}
+            />
+        );
+    }
+
+    render() {
+        while (this.state.buttons.length > 0) {
+            this.state.buttons.pop();
+        }
+        for (var i = 0; i < BASIC_LAYOUT.length; i++) {
+            this.state.buttons.push(this.renderButton(BASIC_LAYOUT[i]));
+        }
+        return (
+            <div className="button-grid">
+                {this.state.buttons}
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            onOperand: false,
+            operand: 0,
+            operator: null,
+            displayContent: "0"
+        }
+    }
+
+    handleClick(buttonType) {
+        var content = this.state.displayContent;
+        if (buttonType.type === "clear") {
+            this.setState({
+                onOperand: false,
+                operand: 0,
+                operator: null,
+                displayContent: "0"
+            });
+        } else if (buttonType.type === "number") {
+            if (content === "0" || this.state.onOperand) {
+                if (buttonType.content === ".") {
+                    content = "0."
+                } else {
+                    content = buttonType.content;
+                }
+            } else {
+                content += buttonType.content;
+            }
+            this.setState({
+                displayContent: content,
+                onOperand: false
+            });
+        } else if (buttonType.type === "oper") {
+            this.setState({
+                onOperand: true,
+                operand: parseFloat(content),
+                operator: buttonType
+            });
+        } else if (buttonType.type === "equal") {
+            if (this.state.operator !== null) {
+                if (this.state.operator === BUTTONS.ADD) {
+                    content = this.state.operand + parseFloat(content);
+                } else if (this.state.operator === BUTTONS.SUBTRACT) {
+                    content = this.state.operand - parseFloat(content);
+                } else if (this.state.operator === BUTTONS.MULTIPLY) {
+                    content = this.state.operand * parseFloat(content);
+                } else if (this.state.operator === BUTTONS.DIVIDE) {
+                    content = this.state.operand / parseFloat(content);
+                }
+
+                this.setState({
+                    operator: null,
+                    displayContent: content
+                });
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div id="calculator">
+                <Display content={this.state.displayContent}/>
+                <div id="title">
+                    <div id="illinois-logo">
+                        <img src="illinois.png" alt=""/>
+                    </div>
+                    <div id="illinois-text">
+                        Illinois Instruments
+                    </div>
+                </div>
+                <ButtonGrid onClick={(buttonType) => this.handleClick(buttonType)}/>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
